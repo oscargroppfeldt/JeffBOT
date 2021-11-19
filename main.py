614a29f73e7b4147d6761d8cfddb7dd559803694
oscargroppfeldt <76077012+oscargroppfeldt@users.noreply.github.com>
2021-11-19 17:43:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+TOKEN = os.environ['TOKEN']
+
+bonk_lst = []
+knownUserAlias = {}
+
+@bot.event
+async def on_ready():
+	print('Logged in as')
+	print(bot.user.name)
+	print(bot.user.id)
+	print('------')
+
+
+@bot.event
+async def on_member_join(ctx, user: discord.Member):
+	if user in bonk_lst:
+		bonk(ctx, user)
+
+@bot.command()
+async def test(ctx,arg="ful"):
+	await ctx.send(f"näe du är {arg}")
+
+@bot.command()
+async def bonk(ctx, user_str):
+
+	if user_str in knownUserAlias:
+		user = knownUserAlias[user_str]
+	else:
+		try:
+			user = ctx.guild.get_member_named(user_str)
+		except TypeError:
+			user = user_str
+
+	if user.id == 138687184302637058 and ctx.message.author.id != 138687184302637058:
+		await ctx.send("Mmm jo tjena va")
+		new_bonk = ctx.message.author
+		await bonk(ctx, new_bonk)
+		return
+
+
+	voice_state = user.voice
+	try:
+		if voice_state is None:
+			await ctx.send(str(user).split('#')[0] + " är ju inte här, bonkar honom när han kommer tillbaka\n ;)")
+			bonk_lst.append(user)
+
+		elif user.voice.channel:
+			if user_str in knownUserAlias:
+				await ctx.send(f"Nu jävlar ska {user_str} få mysa lite med mig")
+				await user.move_to(ctx.guild.afk_channel)
+			else:
+				await ctx.send(f"Nu jävlar ska {str(user).split('#')[0]} få lär kännna mig lite bättre")
+				await user.move_to(ctx.guild.afk_channel)
+	except discord.ext.commands.errors.MemberNotFound:
+		await ctx.send("Vem fan är det?")
+
+@bot.command()
+async def addAlias(ctx, user: discord.Member, arg: str):
+	knownUserAlias[arg] = user
+	await ctx.send(f"HAHAHA kallar ni {str(user).split('#')[0]} för {arg}?\nAja okej då")
+	
+
+bot.run(TOKEN)
